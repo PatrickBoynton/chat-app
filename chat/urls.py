641a4399cf7e95b1784cd7329f0020c319448b71
PatrickBoynton <jpboynton3@gmail.com>
2021-02-24 16:48:05 -0500

    Added update and delete endpoints.

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import ChatListAPIView, ChatDetailAPIView
+from .views import ChatListAPIView, \
+    ChatDetailAPIView, \
+    ChatDeleteAPIView, \
+    ChatUpdateAPIView
 
 app_name = "chat"
 
 urlpatterns = [
     path("<int:pk>/", ChatDetailAPIView.as_view(), name="chat_detail"),
+    path("<int:pk>/delete/", ChatDeleteAPIView.as_view(), name="chat_detail"),
+    path("<int:pk>/update/", ChatUpdateAPIView.as_view(), name="chat_detail"),
     path("", ChatListAPIView.as_view(), name="home")
 ]
