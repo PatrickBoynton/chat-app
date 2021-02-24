00ac2e596a9af86a1d1d24cbc3a0127a846055a5
PatrickBoynton <jpboynton3@gmail.com>
2021-02-24 16:20:32 -0500

    Added a detail view.

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ChatListAPIView
+from .views import ChatListAPIView, ChatDetailAPIView
 
 app_name = "chat"
 
 urlpatterns = [
+    path("<int:pk>/", ChatDetailAPIView.as_view(), name="chat_detail"),
     path("", ChatListAPIView.as_view(), name="home")
 ]
