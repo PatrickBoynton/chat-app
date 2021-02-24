1e3d5591bbb0e1948cb69b54a0642cb43e293414
PatrickBoynton <jpboynton3@gmail.com>
2021-02-24 14:27:53 -0500

    Ran a migration.

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Chat(models.Model):
+    name = models.CharField(max_length=50)
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
