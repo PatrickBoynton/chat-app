c63e2669efce13ce99c381c8e92003302c42c01f
PatrickBoynton <jpboynton3@gmail.com>
2021-02-24 22:25:49 -0500

    Added a simple form and way of displaying the results.

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -3,14 +3,28 @@ import {Component} from "react";
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        fetch("http://127.0.0.1:8000/api/v1/chat")
+            .then(response => response.json())
+            .then(data => data.map(x => console.log(x)))
+    }
+
     render() {
         return (<>
             <div className="App">
                 <h1>Chat App</h1>
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name"/>
-                <label htmlFor="text">Share your thoughts!</label>
-                <input type="text" name="text" id="text"/>
+                <form action="">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" id="name"/>
+                    <label htmlFor="text">Share your thoughts!</label>
+                    <input type="text" name="text" id="text"/>
+                    <button type="submit">Chat!</button>
+                </form>
+
                 <p>Name</p>
                 <h1>Title</h1>
                 <p>Text</p>
