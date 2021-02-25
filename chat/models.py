0854eb2b3b4dde338886ef8f0033a7f8e3c44c45
PatrickBoynton <jpboynton3@gmail.com>
2021-02-24 22:08:10 -0500

    Added a title.

--- chat/models.py
+++ chat/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Chat(models.Model):
-    name = models.CharField(max_length=50)
-    text = models.CharField(max_length=255)
+    name = models.CharField(max_length=50, default="default_name")
+    title = models.CharField(max_length=100, default="default_title")
+    text = models.CharField(max_length=255, default="default_text")
 
     def __str__(self):
         return self.name
