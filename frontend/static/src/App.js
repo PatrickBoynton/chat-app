e0bacf4ee03a3ff2fcca96cb128f5ec00fc7c65a
PatrickBoynton <jpboynton3@gmail.com>
2021-02-24 22:43:25 -0500

    Removed default styling and added a fetch.

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -13,11 +13,15 @@ class App extends Component {
             .then(data => data.map(x => console.log(x)))
     }
 
+    handleSubmit() {
+        return fetch("http://127.0.0.1:8000/api/v1/chat/")
+    }
+
     render() {
         return (<>
             <div className="App">
                 <h1>Chat App</h1>
-                <form action="">
+                <form action="" onSubmit={this.handleSubmit}>
                     <label htmlFor="name">Name</label>
                     <input type="text" name="name" id="name"/>
                     <label htmlFor="text">Share your thoughts!</label>
