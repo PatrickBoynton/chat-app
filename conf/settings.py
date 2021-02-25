4123785ee0dee81afffa85ceb09a461d07ed08a6
PatrickBoynton <jpboynton3@gmail.com>
2021-02-25 00:22:06 -0500

    Added the new url to the allowed hosts.

--- conf/settings.py
+++ conf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost",
+ALLOWED_HOSTS = ["127.0.0.1", "127.0.0.1:8000", 'localhost',
                  "full-stack-chat-app-jp3.herokuapp"]
 
 # Application definition
