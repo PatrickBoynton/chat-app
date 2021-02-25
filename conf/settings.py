f3ae0ee7eaaf5f34e503d7c995c96e525dc81d21
PatrickBoynton <jpboynton3@gmail.com>
2021-02-25 00:05:44 -0500

    Removed more of the extra bits.

--- conf/settings.py
+++ conf/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost",
-                 "full-stack-chat-app-jp3.herokuapp.com"]
+                 "full-stack-chat-app-jp3.herokuapp"]
 
 # Application definition
 
