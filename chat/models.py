a1f31bcee19f9736107ce65da2af4be9ced66d1f
PatrickBoynton <jpboynton3@gmail.com>
2021-02-26 13:18:32 -0500

    Updated the model to include the owner.

--- chat/models.py
+++ chat/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -6,6 +7,7 @@ class Chat(models.Model):
     name = models.CharField(max_length=50)
     title = models.CharField(max_length=100)
     text = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
