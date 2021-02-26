65b725ffbcd78f24c7c979091d44f4d5a4d4c688
PatrickBoynton <jpboynton3@gmail.com>
2021-02-26 14:37:51 -0500

    Added the login form.

--- frontend/static/src/components/Login.js
+++ frontend/static/src/components/Login.js
@@ -2,7 +2,19 @@ import {Component} from 'react'
 
 class Login  extends Component{
     render() {
-        return <div>Login</div>
+        return (<>
+            <h2>Login</h2>
+            <form action="">
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username"/>
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email"/>
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password"/>
+                <button type="submit">Login</button>
+                <p>Don't have an account? Why not <a href="#">Register</a>first?</p>
+            </form>
+        </>)
     }
 }
 
