489afe3caf82bc7195a4a9699930ed970628da4f
PatrickBoynton <jpboynton3@gmail.com>
2021-02-25 22:16:42 -0500

    Added props.

--- frontend/static/src/components/FormDisplay.js
+++ frontend/static/src/components/FormDisplay.js
@@ -6,13 +6,15 @@ class FormDisplay extends Component {
     }
 
     render() {
+        console.log(this.props);
+
         const chats = this.props.chat?.map((x) => (
             <li key={x.id}>
                 <p>{x.name}</p>
                 <h1>{x.title}</h1>
                 <p>{x.text}</p>
-                <button onClick={(event) => this.handleEdit(x.id, event)}>Edit</button>
-                <button onClick={() => this.handleDelete(x.id, this.state)}>Delete</button>
+                <button onClick={(event) => this.props.handleEdit(x.id, event)}>Edit</button>
+                <button onClick={() => this.props.handleDelete(x.id, this.state)}>Delete</button>
             </li>));
         return (
             <div className="chat-display">
