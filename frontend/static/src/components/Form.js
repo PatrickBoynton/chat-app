680ebffcbdf21ef88f8a6d32f760827427f6fb88
PatrickBoynton <jpboynton3@gmail.com>
2021-02-25 20:18:08 -0500

    Changed props to state.

--- frontend/static/src/components/Form.js
+++ frontend/static/src/components/Form.js
@@ -10,18 +10,18 @@ class Form extends Component {
             <form action="" onSubmit={(e) => this.handleSubmit(e)}>
                 <label htmlFor="name">Name</label>
                 <input type="text"
-                       value={this.state.chat.name}
+                       value={this.props.chat.name}
                        name="name"
                        id="name"
                        onChange={this.handleInput}/>
                 <label htmlFor="title">Title</label>
                 <input type="text"
-                       value={this.state.chat.text}
+                       value={this.props.chat.text}
                        name="title"
                        onChange={this.handleInput}/>
                 <label htmlFor="text">Share your thoughts!</label>
                 <input type="text"
-                       value={this.state.chat.text}
+                       value={this.props.chat.text}
                        name="text"
                        onChange={this.handleInput}/>
                 <button type="submit">Chat!</button>
