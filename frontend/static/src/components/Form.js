1887f348fa05644eaf1cac4fae4e3df249ce8781
PatrickBoynton <jpboynton3@gmail.com>
2021-02-25 22:16:16 -0500

    Swapped state with props

--- frontend/static/src/components/Form.js
+++ frontend/static/src/components/Form.js
@@ -7,23 +7,22 @@ class Form extends Component {
 
     render() {
         return (
-            <form action="" onSubmit={(e) => this.handleSubmit(e)}>
+            <form action="" onSubmit={(e) => this.props.handleSubmit(e)}>
                 <label htmlFor="name">Name</label>
                 <input type="text"
-                       value={this.props.chat.name}
+                       value={this.props.chat?.name}
                        name="name"
-                       id="name"
-                       onChange={this.handleInput}/>
+                       onChange={this.props.handleInput}/>
                 <label htmlFor="title">Title</label>
                 <input type="text"
-                       value={this.props.chat.text}
+                       value={this.props.chat?.text}
                        name="title"
-                       onChange={this.handleInput}/>
+                       onChange={this.props.handleInput}/>
                 <label htmlFor="text">Share your thoughts!</label>
                 <input type="text"
-                       value={this.props.chat.text}
+                       value={this.props.chat?.text}
                        name="text"
-                       onChange={this.handleInput}/>
+                       onChange={this.props.handleInput}/>
                 <button type="submit">Chat!</button>
             </form>
         )
