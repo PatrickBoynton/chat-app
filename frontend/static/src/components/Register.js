6cc30853879f419d4a0e5e0baa1e587ea55badd4
PatrickBoynton <jpboynton3@gmail.com>
2021-02-26 14:47:14 -0500

    Added the register form.

--- frontend/static/src/components/Register.js
+++ frontend/static/src/components/Register.js
@@ -2,7 +2,23 @@ import {Component} from "react";
 
 class Register extends Component{
 render() {
-    return <div>Register</div>
+    return (
+        <>
+            <h2>Register</h2>
+        <form action="">
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" />
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email"/>
+            <label htmlFor="password1">Password</label>
+            <input type="password" name="password1"/>
+            <label htmlFor="password2">Confirm Password</label>
+            <input type="password" name="password2"/>
+            <button type="submit">Register</button>
+            <p>Already have an account? Why not <a href="#">Login</a>?</p>
+        </form>
+        </>
+    )
 }
 }
 
