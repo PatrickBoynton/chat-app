e7045d81b6dc0087dd1ac7892cdb5e419dfc50ca
PatrickBoynton <jpboynton3@gmail.com>
2021-02-26 23:49:15 -0500

    Changed props to state.

--- frontend/static/src/components/Register.js
+++ frontend/static/src/components/Register.js
@@ -3,14 +3,12 @@ import {Component} from "react";
 class Register extends Component {
     constructor(props) {
         super(props);
-        // this.state ={
-        //     user: {
-        //         username: "",
-        //         email: "",
-        //         password1: "",
-        //         password2: ""
-        //     }
-        // }
+        this.state ={
+                username: "",
+                email: "",
+                password1: "",
+                password2: ""
+        }
         this.handleInput = this.handleInput.bind(this);
     }
 
@@ -22,26 +20,27 @@ class Register extends Component {
         return (
             <>
                 <h2>Register</h2>
-                <form action="" onSubmit={(e) => this.props.handleRegistration(e, this.props.user)}>
+                <form action="" onSubmit={(e) =>
+                    this.props.handleRegistration(e, this.state)}>
                     <label htmlFor="username">Username</label>
                     <input type="text"
                            name="username"
-                           value={this.props.user?.username}
+                           value={this.state.username}
                            onChange={this.handleInput}/>
                     <label htmlFor="email">Email</label>
                     <input type="email"
                            name="email"
-                           value={this.props.user?.email}
+                           value={this.state.email}
                            onChange={this.handleInput}/>
                     <label htmlFor="password1">Password</label>
                     <input type="password"
                            name="password1"
-                           value={this.props.user?.password1}
+                           value={this.state.password1}
                            onChange={this.handleInput}/>
                     <label htmlFor="password2">Confirm Password</label>
                     <input type="password"
                            name="password2"
-                           value={this.props.user?.password2}
+                           value={this.state.password2}
                            onChange={this.handleInput}/>
                     <button type="submit">Register</button>
                     <p>Already have an account? Why not <a href="/">Login</a>?</p>
