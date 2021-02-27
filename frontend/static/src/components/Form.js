eb23d5c65469c9483f1418e36dc0ff13ba96f684
PatrickBoynton <jpboynton3@gmail.com>
2021-02-27 17:04:33 -0500

    Moved the chat form here.

--- frontend/static/src/components/Form.js
+++ frontend/static/src/components/Form.js
@@ -1,29 +1,24 @@
-import {Component} from "react";
-
-class Form extends Component {
-
-    render() {
-        return (
-            <form action="" onSubmit={(e) => this.props.handleSubmit(e)}>
-                <label htmlFor="name">Name</label>
-                <input type="text"
-                       value={this.props.chat?.name}
-                       name="name"
-                       onChange={this.props.handleInput}/>
-                <label htmlFor="title">Title</label>
-                <input type="text"
-                       value={this.props.chat?.text}
-                       name="title"
-                       onChange={this.props.handleInput}/>
-                <label htmlFor="text">Share your thoughts!</label>
-                <input type="text"
-                       value={this.props.chat?.text}
-                       name="text"
-                       onChange={this.props.handleInput}/>
-                <button type="submit">Chat!</button>
-            </form>
-        )
-    }
+function Form(props) {
+    return (
+        <form action="" onSubmit={(e) => props.handlePost(e)}>
+            <label htmlFor="name">Name</label>
+            <input type="text"
+                   value={props.chat?.name}
+                   name="name"
+                   onChange={props.handleInput}/>
+            <label htmlFor="title">Title</label>
+            <input type="text"
+                   value={props.chat?.text}
+                   name="title"
+                   onChange={props.handleInput}/>
+            <label htmlFor="text">Share your thoughts!</label>
+            <input type="text"
+                   value={props.chat?.text}
+                   name="text"
+                   onChange={props.handleInput}/>
+            <button type="submit">Chat!</button>
+        </form>
+    )
 }
 
 export default Form
