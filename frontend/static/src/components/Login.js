c5e54ce515f4b2d2bb40a89f624e80c1ad2a9dd3
PatrickBoynton <jpboynton3@gmail.com>
2021-02-27 18:43:54 -0500

    Refactored and added onChange.

--- frontend/static/src/components/Login.js
+++ frontend/static/src/components/Login.js
@@ -1,16 +1,34 @@
-import {Component} from 'react'
+import {Component} from "react";
 
-class Login  extends Component{
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            email: "",
+            password: ""
+        }
+    }
+
     render() {
         return (<>
             <h2>Login</h2>
-            <form action="">
+            <form action="" onSubmit={(e) => this.props.handleLogin(e, this.props)}>
                 <label htmlFor="username">Username</label>
-                <input type="text" name="username"/>
+                <input type="text"
+                       name="username"
+                       value={this.props.username}
+                       onChange={this.props.handleInput}/>
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email"/>
+                <input type="email"
+                       name="email"
+                       value={this.props.email}
+                       onChange={this.props.handleInput}/>
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password"/>
+                <input type="password"
+                       name="password"
+                       value={this.props.password}
+                       onChange={this.props.handleInput}/>
                 <button type="submit">Login</button>
                 <p>Don't have an account? Why not <a href="/">Register</a>first?</p>
             </form>
