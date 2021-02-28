f91ea99f6db94e133a2686d7d8af769701947fa8
PatrickBoynton <jpboynton3@gmail.com>
2021-02-28 15:06:15 -0500

    Refactored to display login or register first.

--- frontend/static/src/components/Room.js
+++ frontend/static/src/components/Room.js
@@ -3,28 +3,42 @@ import Register from "./Register";
 import Form from "./Form";
 import FormDisplay from "./FormDisplay";
 import {Component} from "react";
+import Cookies from 'js-cookie'
 
 class Room extends Component {
+
     render() {
 
         return (
             <>
-                <Form handleInput={this.props.handleInput}
-                      handlePost={this.props.handlePost}/>
+                {
+                    this.props.isLoggedIn === Cookies.get("Authorization")
+                        ?
+                        <>
+                            <Login user={this.props.user}
+                                   handleLogin={this.props.handleLogin}
+                                   handleInput={this.props.handleInput}/>
+                            {/*Remember to change it to props when passing it down not state.*/}
+                            <Register user={this.props.user}
+                                      handleRegistration={this.props.handleRegistration}
+                                      handleInput={this.props.handleInput}/>
+                        </>
+                        :
+                        <>
+                            <Form handleInput={this.props.handleInput}
+                                  handlePost={this.props.handlePost}/>
 
-                <FormDisplay chat={this.props.chat}
-                             handleEdit={this.props.handleEdit}
-                             handleDelete={this.props.handleDelete}/>
-                <Login user={this.props.user}
-                       handleLogin={this.props.handleLogin}
-                       handleInput={this.props.handleInput}/>
-                {/*Remember to change it to props when passing it down not state.*/}
-                <Register user={this.props.user}
-                          handleRegistration={this.props.handleRegistration}
-                          handleInput={this.props.handleInput}/>
+                            <FormDisplay chat={this.props.chat}
+                                         handleEdit={this.props.handleEdit}
+                                         handleDelete={this.props.handleDelete}/>
+                            <button onClick={(e) => this.props.handleLogout(e)}>Logout</button>
+                        </>
+
+                }
             </>
         )
     }
+
 }
 
 export default Room
