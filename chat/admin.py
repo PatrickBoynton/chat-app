8ca69a000ff8850159f11afaa6a5f3c82b771f6b
PatrickBoynton <jpboynton3@gmail.com>
2021-03-06 23:07:30 -0500

    Registered the room model.

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Chat
+from . import models
 
 # Register your models here.
-admin.site.register(Chat)
+admin.site.register(models.Chat)
+admin.site.register(models.Room)
