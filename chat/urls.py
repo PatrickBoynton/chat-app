0c418f650084f4c20508a415cd03327342bbd1f4
PatrickBoynton <jpboynton3@gmail.com>
2021-03-06 23:09:04 -0500

    Added a room path, and formatted.

--- chat/urls.py
+++ chat/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from .views import ChatListAPIView, \
-    ChatDetailAPIView, \
-    ChatDeleteAPIView, \
-    ChatUpdateAPIView
+from . import views
 
 app_name = "chat"
 
 urlpatterns = [
-    path("<int:pk>/", ChatDetailAPIView.as_view(), name="chat_detail"),
-    path("<int:pk>/delete/", ChatDeleteAPIView.as_view(), name="chat_detail"),
-    path("<int:pk>/update/", ChatUpdateAPIView.as_view(), name="chat_detail"),
-    path("", ChatListAPIView.as_view(), name="home")
+    path("rooms", views.RoomListView.as_view(), name="room_detail"),
+    path("<int:pk>/", views.ChatDetailAPIView.as_view(), name="chat_detail"),
+    path("<int:pk>/delete/", views.ChatDeleteAPIView.as_view(),
+         name="chat_delete"),
+    path("<int:pk>/update/", views.ChatUpdateAPIView.as_view(),
+         name="chat_update"),
+    path("", views.ChatListAPIView.as_view(), name="home")
 ]
