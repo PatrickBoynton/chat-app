bc9a7964a67a766da011aaec8329561109ea707d
PatrickBoynton <jpboynton3@gmail.com>
2021-03-07 16:26:10 -0500

    Gave x a more meaningful name.

--- frontend/static/src/components/ChatDisplay.js
+++ frontend/static/src/components/ChatDisplay.js
@@ -37,13 +37,13 @@ class ChatDisplay extends Component {
     }
 
     render() {
-        const chats = this.props.chat?.map((x) => (
-            <li key={x.id} onClick={() => console.log(x.id)}>
-                <p>{x.name}</p>
-                <h1>{x.title}</h1>
-                <p>{x.text}</p>
-                <button onClick={(event) => this.props.handleEdit(x.id, event)}>Edit</button>
-                <button onClick={() => this.props.handleDelete(x.id, this.state)}>Delete</button>
+        const chats = this.props.chat?.map((chat) => (
+            <li key={chat.id} onClick={() => console.log(chat.id)}>
+                <p>{chat.name}</p>
+                <h1>{chat.title}</h1>
+                <p>{chat.text}</p>
+                <button onClick={(event) => this.props.handleEdit(chat.id, event)}>Edit</button>
+                <button onClick={() => this.props.handleDelete(chat.id, this.state)}>Delete</button>
             </li>));
         return (
             <div className="chat-display">
