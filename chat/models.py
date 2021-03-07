e9fc6f4bfe08a078f76b53058fd933b8a3873ebc
PatrickBoynton <jpboynton3@gmail.com>
2021-03-06 23:07:50 -0500

    Added a room model, removed the title from the chat.

--- chat/models.py
+++ chat/models.py
@@ -2,17 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Room
-class Room(models.Model):
-    title = models.CharField(max_length=50)
-
-
 # Create your models here.
 class Chat(models.Model):
     name = models.CharField(max_length=50)
-    title = models.CharField(max_length=100)
     text = models.CharField(max_length=255)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
+
+
+# Room
+class Room(models.Model):
+    title = models.CharField(max_length=50, null=True)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
