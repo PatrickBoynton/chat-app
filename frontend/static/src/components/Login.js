3a7b622964591753b6710c46de869362aeb0d623
PatrickBoynton <jpboynton3@gmail.com>
2021-03-07 16:26:57 -0500

    Refactored the login.

--- frontend/static/src/components/Login.js
+++ frontend/static/src/components/Login.js
@@ -1,39 +1,91 @@
-import {Component} from "react";
+import {Component} from 'react';
+import Cookies from 'js-cookie';
 
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: "",
-            email: "",
-            password: ""
+            username: '',
+            email: '',
+            password: ''
+        };
+        this.handleInput = this.handleInput.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+    componentDidMount() {
+        if (this.state.isLoggedIn) {
+            const options = {
+                headers: {
+                    'Content-Type': 'Application/Json',
+                    'X-CSRFToken': Cookies.get('csrftoken'),
+                    'Authorization': Cookies.get('Authorization')
+                }
+            };
+            console.log(options);
+            fetch('/api/v1/chat', options)
+                .then(response => response.json())
+                .then(data => this.setState({chat: [...data]}));
+            console.log('Logged in.');
+        } else {
+            console.log('Logged out.');
+        }
+    }
+
+    handleInput(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    async handleLogin(e, object) {
+        e.preventDefault();
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'Application/JSON',
+                'X-CSRFToken': Cookies.get('csrftoken'),
+            },
+            body: JSON.stringify({
+                username: object.username,
+                email: object.email,
+                password: object.password
+            }),
+        };
+        const response = await fetch('/rest-auth/login/', options);
+        const data = await response.json().catch(error => console.log(error));
+        console.log(data);
+        if (data.key) {
+            Cookies.set('Authorization', `Token ${data.key}`);
+            this.setState({isLoggedIn: !!Cookies.get('Authorization')});
+        } else {
+            console.log('Did not log in.');
         }
     }
 
     render() {
         return (<>
             <h2>Login</h2>
-            <form action="" onSubmit={(e) => this.props.handleLogin(e, this.props)}>
+            <form action="" onSubmit={(e) => this.handleLogin(e, this.state)}>
                 <label htmlFor="username">Username</label>
                 <input type="text"
                        name="username"
-                       value={this.props.username}
-                       onChange={this.props.handleInput}/>
+                       value={this.state.username}
+                       onChange={this.handleInput}/>
                 <label htmlFor="email">Email</label>
                 <input type="email"
                        name="email"
-                       value={this.props.email}
-                       onChange={this.props.handleInput}/>
+                       value={this.state.email}
+                       onChange={this.handleInput}/>
                 <label htmlFor="password">Password</label>
                 <input type="password"
                        name="password"
-                       value={this.props.password}
-                       onChange={this.props.handleInput}/>
+                       value={this.state.password}
+                       onChange={this.handleInput}/>
                 <button type="submit">Login</button>
             </form>
-            <p>Don't have an account? Why not <a onClick={() => this.props.loginOrRegister()} href="#">Register</a>first?</p>
-        </>)
+            <p>Don't have an account? Why not <a onClick={() => this.props.loginOrRegister()} href="#">Register</a>first?
+            </p>
+        </>);
     }
 }
 
-export default Login
+export default Login;
