2a41b552c3d3a1ed367db33b146397574d389c0b
PatrickBoynton <jpboynton3@gmail.com>
2021-03-06 23:08:44 -0500

    Added a room serializer.

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from .models import Chat
+from .models import Chat, Room
 
 
 class ChatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chat
-        fields = ('id', 'name', 'title', 'text')
+        fields = ('id', 'name', 'text')
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'title', 'chat')
