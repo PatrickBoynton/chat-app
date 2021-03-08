03e0d17ba21e635e72a14337ed8674eecb8562a6
PatrickBoynton <jpboynton3@gmail.com>
2021-03-07 23:35:26 -0500

    Added a room model, and a foriegn key.

--- chat/models.py
+++ chat/models.py
@@ -3,19 +3,18 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Room(models.Model):
+    title = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Chat(models.Model):
     name = models.CharField(max_length=50)
     text = models.CharField(max_length=255)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
-
-
-# Room
-class Room(models.Model):
-    title = models.CharField(max_length=50, null=True)
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return self.title
