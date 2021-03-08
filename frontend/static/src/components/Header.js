b3ad45c0b1078987d86ea69b9d74cdad944fea9b
PatrickBoynton <jpboynton3@gmail.com>
2021-03-07 23:35:05 -0500

    Setting up rooms.

--- frontend/static/src/components/Header.js
+++ frontend/static/src/components/Header.js
@@ -1,25 +1,31 @@
 import {Component} from 'react';
 
-class Header extends Component{
+class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {
             rooms: [],
-        }
+        };
     }
+
     async componentDidMount() {
         const response = await fetch('/api/v1/chat/rooms');
         const data = await response.json();
-        if(!data.detail) {
-         this.setState({rooms: data});
+        if (!data.detail) {
+            this.setState({rooms: data});
         }
     }
+
     render() {
         const rooms = this.state.rooms?.map(room => <section key={room.id}>
-            <button onClick={() => console.log(room)}>{room.title}</button>
+            <button onClick={() => this.props.handleRoomSwitch(room)}>{room.title}</button>
+
         </section>);
-        return <>{rooms}</>
+        return <>
+            {rooms}
+            <div>{this.state.test}</div>
+        </>;
     }
 }
 
-export default Header
+export default Header;
