8b2067a1d0b8bac2135e260aec84fb684b83ee79
PatrickBoynton <jpboynton3@gmail.com>
2021-03-07 23:38:34 -0500

    Changed the fields to all.

--- chat/serializers.py
+++ chat/serializers.py
@@ -6,10 +6,10 @@ from .models import Chat, Room
 class ChatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chat
-        fields = ('id', 'name', 'text')
+        fields = ('__all__')
 
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ('id', 'title', 'chat')
+        fields = ('__all__')
