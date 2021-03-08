7cb02f85ff8a61d33bc090e51297cf5d1461463f
PatrickBoynton <jpboynton3@gmail.com>
2021-03-07 23:34:39 -0500

    This connects posts to rooms.

--- frontend/static/src/components/ChatDisplay.js
+++ frontend/static/src/components/ChatDisplay.js
@@ -4,26 +4,7 @@ import Cookies from 'js-cookie';
 class ChatDisplay extends Component {
     constructor(props) {
         super(props);
-        this.handleEdit = this.handleEdit.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
-    }
-
-    handleEdit(chat) {
-        fetch('/api/v1/chat/' + chat.id + '/update/', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'Application/Json',
-                'Authorization': Cookies.get('Authorization'),
-                'X-CSRFToken': Cookies.get('csrftoken')
-            },
-            body: JSON.stringify({
-                name: chat.name,
-                text: chat.text
-            })
-        });
-
-        this.setState({name: chat.name, text: chat.title});
-        this.props.handleEditMode();
     }
 
     handleDelete(chat) {
@@ -40,12 +21,25 @@ class ChatDisplay extends Component {
 
     render() {
         const chats = this.props.chat?.map((chat) => (
-            <section key={chat.id} onClick={() => console.log(chat.id)}>
-                <p>{chat.name}</p>
-                <h1>{chat.title}</h1>
-                <p>{chat.text}</p>
-                <button onClick={(event) => this.handleEdit(chat, event)}>Edit</button>
-                <button onClick={() => this.handleDelete(chat.id, this.state)}>Delete</button>
+            <section key={chat.id} onClick={() => {
+                if (chat.name === this.props.user) {
+                    console.log(chat);
+                }
+            }}>
+                {
+                    chat.room === this.props.current_room
+                        ?
+                        <>
+                            <p>{chat.name}</p>
+                            <h1>{chat.title}</h1>
+                            <p>{chat.text}</p>
+                            <button onClick={(event) => this.props.handleEdit(chat, event)}>Edit</button>
+                            <button onClick={() => this.handleDelete(chat.id, this.state)}>Delete</button>
+                        </>
+                        :
+                        null
+                }
+
             </section>));
         return (
             <>
